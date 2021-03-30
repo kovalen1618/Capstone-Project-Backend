8f6f62c27741a55710df80950f2b0dbadc63b005
Kris Newell <knewell062006@gmail.com>
2021-03-30 14:17:15 -0500

    Added QuoteItem Repo and Interface to Capstone

---
+++ Contracts/IQuoteItemRepository.cs
@@ -0,0 +1,17 @@
+﻿using playlist_app_backend.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace playlist_app_backend.Contracts
+{
+    public interface IQuoteItemRepository: IRepositoryBase<QuoteItem>
+    {
+        IEnumerable<QuoteItem> GetAllQuoteItems();
+        QuoteItem GetQuoteItemById(int itemId);
+        void CreateQuoteItem(QuoteItem quoteItem);
+        void UpdateQuoteItem(QuoteItem quoteItem);
+        void DeleteQuoteItem(QuoteItem quoteItem);
+    }
+}
---
+++ Repository/QuoteItemRepository.cs
@@ -0,0 +1,46 @@
+﻿using playlist_app_backend.Contracts;
+using playlist_app_backend.Entities;
+using playlist_app_backend.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace playlist_app_backend.Repository
+{
+    public class QuoteItemRepository : RepositoryBase<QuoteItem>, IQuoteItemRepository
+    {
+        public QuoteItemRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+
+        public void CreateQuoteItem(QuoteItem quoteItem)
+        {
+            Create(quoteItem);
+        }
+
+        public void DeleteQuoteItem(QuoteItem quoteItem)
+        {
+            Delete(quoteItem);
+        }
+
+        public IEnumerable<QuoteItem> GetallQuoteItems()
+        {
+            return FindAll()
+                .OrderBy(pl => pl.Id)
+                .ToList();
+        }
+
+        public QuoteItem GetQuoteItemById(int playlistId)
+        {
+            return FindByCondition(playlist => playlist.Id.Equals(playlistId))
+                .FirstOrDefault();
+        }
+
+        public void UpdateQuoteItem(QuoteItem quoteItem)
+        {
+            Update(quoteItem);
+        }
+    }
+}
